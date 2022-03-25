0e179112baa2e78a89706a09dbbbe580eff4dddd
wa765 <wahyusaputra12345678@gmail.com>
2022-03-25 23:43:09 +0700

    Design Color

---
+++ lib/ColorTheme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// Neon Blue
+Color neonBlue = const Color(0xff1D9BF0);
+
+// White
+Color whiteColor = Colors.white;
+
+//  Dark
+Color darkColor = const Color(0xff15202B);
+
+// Grey
+Color greyColor = const Color(0xffAAB8C2);
+
+// 
