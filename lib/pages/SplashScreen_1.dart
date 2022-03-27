850fa10d79b7321d07075be28916f2c3c8124a22
wa765 <wahyusaputra12345678@gmail.com>
2022-03-28 02:03:01 +0700

    Done : Splash Screen 1

--- lib/pages/SplashScreen_1.dart
+++ lib/pages/SplashScreen_1.dart
@@ -1,12 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dicoding/text_style.dart';
 
 class SplashScreen1 extends StatelessWidget {
   const SplashScreen1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xff1C212B),
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 40),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/image_splash_1.png",
+                  width: 207,
+                  height: MediaQuery.of(context).size.height / 2.2,
+                ),
+                Flexible(
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 48),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    decoration: const BoxDecoration(
+                      color: Color(0xff253341),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Discover Largest \nNFT MarketPlace",
+                          textAlign: TextAlign.center,
+                          style: mainHeading.copyWith(
+                              color: Colors.white, fontSize: 34),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          "Buy and sell digital items",
+                          style: mainSubtitle,
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 10,
+                              height: 10,
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.white),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              width: 10,
+                              height: 10,
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Color(0xFF1C212B)),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              width: 10,
+                              height: 10,
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Color(0xFF1C212B)),
+                            ),
+                          ],
+                        ),
+                        const Spacer(),
+                        Container(
+                          margin: const EdgeInsets.only(right: 24, left: 24),
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Text(
+                              "Next",
+                              style: mainSubtitle.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
