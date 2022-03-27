695bfe571bd951ffd56fff56bdcd4ad0e868d20b
wa765 <wahyusaputra12345678@gmail.com>
2022-03-28 02:02:28 +0700

    style text

---
+++ lib/text_style.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'ColorTheme.dart';
+
+// main Heading
+TextStyle mainHeading = TextStyle(
+  color: darkColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+);
+
+// Secondary Heading
+TextStyle secondaryHeading = TextStyle(
+  color: darkColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+// description Heading
+TextStyle mainSubtitle =
+    TextStyle(color: greyColor, fontSize: 16, fontStyle: FontStyle.normal);
+
+// Text Main Button
+TextStyle mainButton = TextStyle(
+  color: whiteColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+
+// Text White Collor
+TextStyle whiteFont = TextStyle(
+  color: whiteColor,
+  fontWeight: FontWeight.w500,
+);
